30166762c264dd27598a5a96d86067b22ea517f7
Rajabov Amirjon <109369733+amigo0557@users.noreply.github.com>
2022-11-12 18:32:05 +0500

    Add files via upload

---
+++ TraslatorBot/config.py
@@ -0,0 +1,21 @@
+
+
+TOKEN = '5798105471:AAGqVNIETl2bKv4SmWOtb0USDKFVX_2w54s'
+
+
+
+STARTMSG = "Men Tarjimon botman👋 "
+CHOOSEMSG = "Matnni qaysi tilga tarjima qilmoqchisiz?"
+
+
+LANGDICT = {
+
+    'uz': '🇺🇿Uzbek',
+    'nl': '🇳🇱Dutch',
+    'en': '🇺🇸English',
+    'fr': '🇫🇷French',
+    'ko': '🇰🇷Korean',
+    'ru': '🇷🇺Russian',
+
+  }
+LANGUES = LANGDICT.keys()
